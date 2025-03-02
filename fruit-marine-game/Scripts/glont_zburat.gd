f71383f2a15311ad159ca0aed43c9f79baeec69f
octavi26 <dimitrie-octavian.radu@s.unibuc.ro>
2025-03-03 00:34:40 +0200

    Animations
    Am adaugat animatii pt mers si idle.
    Atunci cand isi schimba directia playeru is da si flip ca sa traga in directia potrivita.
    Nota pt mine, adauga un buffer pt jump si schimba puterea gravitatiei cand preierul cade.

--- fruit-marine-game/Scripts/glont_zburat.gd
+++ fruit-marine-game/Scripts/glont_zburat.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-var speed = 30
+var speed = 500
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
--- fruit-marine-game/Scripts/player_movement.gd
+++ fruit-marine-game/Scripts/player_movement.gd
@@ -5,17 +5,33 @@ var scene = load("res://Scenes/glont_template.tscn")
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var orientation = 1
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 	if Input.is_action_pressed("shoot") and $Timer.is_stopped():
 		shoot()
-	
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	#Handle Animations
+	if $Timer.is_stopped():
+		$TopSprite.play("Idle")
+	
+	
+	if direction != 0 and direction != orientation:
+		self.scale.x *= -1
+		orientation *= -1
+		
+	if direction == 0:
+		$BottomSprite.play("Idle")
+	else:
+		$BottomSprite.play("Running")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -23,6 +39,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func shoot():
+	$TopSprite.play("Shooting")
+	
 	var b = scene.instantiate()
 	owner.add_child(b)
 	b.global_transform = $Marker2D.global_transform
