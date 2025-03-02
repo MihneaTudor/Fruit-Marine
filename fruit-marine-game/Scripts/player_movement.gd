a9e0889714ffcedac518bfb4ea680a95b525951a
MihneaTudor <78757753+MihneaTudor@users.noreply.github.com>
2025-03-02 20:56:32 +0100

    Movement+shooting(Base)

---
+++ fruit-marine-game/Scripts/player_movement.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+var scene = load("res://Scenes/glont_template.tscn")
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_pressed("shoot") and $Timer.is_stopped():
+		shoot()
+	
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	
+func shoot():
+	var b = scene.instantiate()
+	owner.add_child(b)
+	b.global_transform = $Marker2D.global_transform
+	$Timer.start()
