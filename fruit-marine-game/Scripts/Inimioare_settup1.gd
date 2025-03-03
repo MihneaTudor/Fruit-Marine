67862eccfa52d8ea67f3176faefa339aa5b0d193
MihneaTudor <78757753+MihneaTudor@users.noreply.github.com>
2025-03-03 18:18:24 +0100

    Inimiaore update

---
+++ fruit-marine-game/Scripts/Inimioare_settup1.gd
@@ -0,0 +1,15 @@
+extends HBoxContainer
+
+@export var HeartFull: Texture
+@export var HeartEmpty: Texture
+@export var player: CharacterBody2D  # Assign player in the editor
+
+func _ready():
+	player.health_changed.connect(update_hearts)
+	update_hearts(player.current_health)
+
+func update_hearts(health):
+	for i in range(get_child_count()):
+		var heart = get_child(i)
+		if heart is TextureRect:
+			heart.texture = HeartFull if i < health else HeartEmpty
